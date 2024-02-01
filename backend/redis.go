8afa9eade5271f79d8e71ca19ed09f8cf033a759
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-02-01 14:08:47 +0000

    Feat(redis): Change 127... to redis:
    Docker containers don't like when you use `localhost` or `127.0.0.1` as it's that IP within the container so you must use the name of the container (as per `docker-compose.yml`'s service which is redis)

--- backend/redis.go
+++ backend/redis.go
@@ -4,7 +4,7 @@ import (
 	"github.com/redis/rueidis"
 )
 func connect() (rueidis.Client, error) {
-	client, err := rueidis.NewClient(rueidis.ClientOption{InitAddress: []string{"127.0.0.1:6379"}})
+	client, err := rueidis.NewClient(rueidis.ClientOption{InitAddress: []string{"redis:6379"}})
 	if err != nil {
 		return nil, err
 	}
